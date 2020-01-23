80e5e0f2be3a69bcfe58a937be6f490b681f6ed6
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-01-23 21:09:52 +0100

    Added main.c file

---
+++ Src/main.c
@@ -0,0 +1,18 @@
+/*
+ * 001_Printf.c
+ *
+ *  Created on: Jan 23, 2020
+ *      Author: nemanja
+ */
+
+#include "stm32f446xx.h"
+
+#if !defined(__SOFT_FPU__) && defined(__ARM_FP)
+	#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize FPU before use."
+#endif
+
+
+int main(void)
+{
+	return 0;
+}
