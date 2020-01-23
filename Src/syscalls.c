99bf489d0f01a39ffe62f70726ee9fd398f67185
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-01-23 19:38:04 +0100

    ITM implementation

--- Src/syscalls.c
+++ Src/syscalls.c
@@ -56,6 +56,36 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//					Implementation of printf like feature using ARM Cortex M3/M4/M7 ITM functionality
+//					This function will not work for ARM Cortex M0/M0+
+//					If you are using Cortex M0, then you can use semihosting feature of openOCD
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Debug Exception and Monitor Control Register base address
+#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
+
+/* ITM register addresses */
+#define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
+#define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
+
+void ITM_SendChar(uint8_t ch)
+{
+
+	//Enable TRCENA
+	DEMCR |= ( 1 << 24);
+
+	//enable stimulus port 0
+	ITM_TRACE_EN |= ( 1 << 0);
+
+	// read FIFO status in bit [0]:
+	while(!(ITM_STIMULUS_PORT0 & 1));
+
+	//Write to ITM stimulus port0
+	ITM_STIMULUS_PORT0 = ch;
+}
+/* END of ITM implementation */
 
 /* Variables */
 //#undef errno
@@ -109,7 +139,8 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
 	for (DataIdx = 0; DataIdx < len; DataIdx++)
 	{
-		__io_putchar(*ptr++);
+		//__io_putchar(*ptr++);
+		ITM_SendChar(*ptr++);	//ITM Debuger enabled
 	}
 	return len;
 }
