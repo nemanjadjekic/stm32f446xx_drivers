4387154ac1b4a8a5f6db12df9e8fea172b51fda6
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-01-24 23:10:18 +0100

    Added test for LED toggle with button

--- Src/Tests/004_LedToggle_Button.c
+++ Src/Tests/004_LedToggle_Button.c
@@ -1,5 +1,5 @@
 /*
- * 003_LedToggle_OpenDrainCfg.c
+ * 004_LedToggle_Button.c
  *
  *  Created on: Jan 24, 2020
  *      Author: nemanja
@@ -11,6 +11,8 @@
 	#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize FPU before use."
 #endif
 
+#define BTN_PRESSED 0
+
 void delay(void)
 {
 	for(uint32_t i = 0; i < 500000; i++);
@@ -18,23 +20,36 @@ void delay(void)
 
 int main(void)
 {
-	GPIO_Handle_t GpioLed;
+	GPIO_Handle_t GpioLed, GpioBtn;
 
 	GpioLed.pGPIOx = GPIOA;
 	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
 	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
 	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_NO_PUPD;
 
 	GPIO_PeriClockControl(GPIOA, ENABLE);
 
 	GPIO_Init(&GpioLed);
 
+	GpioBtn.pGPIOx = GPIOC;
+	GpioBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GpioBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GpioBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOC, ENABLE);
+
+	GPIO_Init(&GpioBtn);
+
 	while(1)
 	{
-		GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
-		delay();
+		if(GPIO_ReadFromInputPin(GPIOC, GPIO_PIN_NO_13) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+		}
 	}
 
 	return 0;
