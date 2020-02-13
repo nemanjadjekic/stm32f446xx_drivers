166ee760fcc2c81edc81f4fea32c90e3f439e4dc
nemanjadjekic <nemanjadjekic93@gmail.com>
2020-02-13 21:02:16 +0100

    Added documentation and created init i2c .c and .h files

---
+++ Drivers/inc/stm32f446xx_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx_i2c_driver.h
+ *
+ *  Created on: Feb 13, 2020
+ *      Author: nemanja
+ */
+
+#ifndef INC_STM32F446XX_I2C_DRIVER_H_
+#define INC_STM32F446XX_I2C_DRIVER_H_
+
+
+
+#endif /* INC_STM32F446XX_I2C_DRIVER_H_ */
---
+++ Drivers/src/stm32f446xx_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446xx_i2c_driver.c
+ *
+ *  Created on: Feb 13, 2020
+ *      Author: nemanja
+ */
+
+
