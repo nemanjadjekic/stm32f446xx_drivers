f9305455e92548ae14d490def14f6d20e1fcb151
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-03-03 17:23:15 +0100

    Working on alignment of .h and .c files

--- Drivers/inc/stm32f446xx_gpio_driver.h
+++ Drivers/inc/stm32f446xx_gpio_driver.h
@@ -38,22 +38,22 @@ typedef struct
  * @GPIO_PIN_NUMBERS
  * GPIO pin numbers
  */
-#define GPIO_PIN_NO_0	0
-#define GPIO_PIN_NO_1	1
-#define GPIO_PIN_NO_2	2
-#define GPIO_PIN_NO_3	3
-#define GPIO_PIN_NO_4	4
-#define GPIO_PIN_NO_5	5
-#define GPIO_PIN_NO_6	6
-#define GPIO_PIN_NO_7	7
-#define GPIO_PIN_NO_8	8
-#define GPIO_PIN_NO_9	9
-#define GPIO_PIN_NO_10	10
-#define GPIO_PIN_NO_11	11
-#define GPIO_PIN_NO_12	12
-#define GPIO_PIN_NO_13	13
-#define GPIO_PIN_NO_14	14
-#define GPIO_PIN_NO_15	15
+#define GPIO_PIN_NO_0		0
+#define GPIO_PIN_NO_1		1
+#define GPIO_PIN_NO_2		2
+#define GPIO_PIN_NO_3		3
+#define GPIO_PIN_NO_4		4
+#define GPIO_PIN_NO_5		5
+#define GPIO_PIN_NO_6		6
+#define GPIO_PIN_NO_7		7
+#define GPIO_PIN_NO_8		8
+#define GPIO_PIN_NO_9		9
+#define GPIO_PIN_NO_10		10
+#define GPIO_PIN_NO_11		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15		15
 
 
 /*
