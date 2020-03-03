08c7efa56e1b7e3f0ae7db5f89e9b6c977f463ae
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-03-03 17:21:50 +0100

    Working on alignment of .h and .c files

--- Drivers/inc/stm32f446xx_gpio_driver.h
+++ Drivers/inc/stm32f446xx_gpio_driver.h
@@ -15,12 +15,12 @@
  */
 typedef struct
 {
-	uint8_t GPIO_PinNumber;			/* Possible values from @GPIO_PIN_NUMBERS  						   */
-	uint8_t GPIO_PinMode;			/* Possible values from @GPIO_PIN_MODES	   						   */
-	uint8_t GPIO_PinSpeed;			/* Possible values from @GPIO_PIN_SPEED	   						   */
-	uint8_t GPIO_PinPuPdControl;	/* Possible values from @GPIO_PIN_PULL_UP_PULL_DOWN				   */
-	uint8_t GPIO_PinOPType;			/* Possible values from @GPIO_PIN_OUTPUT_TYPE 					   */
-	uint8_t GPIO_PinAltFunMode;		/* Has value only when @GPIO_PIN_MODES is set to Alt functionality */
+	uint8_t GPIO_PinNumber;		/* Possible values from @GPIO_PIN_NUMBERS  			   */
+	uint8_t GPIO_PinMode;		/* Possible values from @GPIO_PIN_MODES	   		           */
+	uint8_t GPIO_PinSpeed;		/* Possible values from @GPIO_PIN_SPEED	   			   */
+	uint8_t GPIO_PinPuPdControl;	/* Possible values from @GPIO_PIN_PULL_UP_PULL_DOWN		   */
+	uint8_t GPIO_PinOPType;		/* Possible values from @GPIO_PIN_OUTPUT_TYPE 			   */
+	uint8_t GPIO_PinAltFunMode;	/* Has value only when @GPIO_PIN_MODES is set to Alt functionality */
 }GPIO_PinConfig_t;
 
 
@@ -29,8 +29,8 @@ typedef struct
  */
 typedef struct
 {
-	GPIO_RegDef_t *pGPIOx;				/* This holds the base address of the GPIO port to which the pin belongs */
-	GPIO_PinConfig_t GPIO_PinConfig;	/* This holds GPIO pin configuration settings 							 */
+	GPIO_RegDef_t *pGPIOx;		 /* This holds the base address of the GPIO port to which the pin belongs */
+	GPIO_PinConfig_t GPIO_PinConfig; /* This holds GPIO pin configuration settings 							 */
 }GPIO_Handle_t;
 
 
@@ -60,21 +60,21 @@ typedef struct
  * @GPIO_PIN_MODES
  * GPIO pin possible modes
  */
-#define GPIO_MODE_IN    	0	/* GPIO Input mode 		  					*/
-#define GPIO_MODE_OUT   	1	/* GPIO Output mode 	  					*/
-#define GPIO_MODE_ALTFN 	2   /* GPIO Alternate functionality 			*/
-#define GPIO_MODE_ANALOG	3	/* GPIO Analog mode       					*/
-#define GPIO_MODE_IT_FT		4 	/* GPIO Input mode falling edge trigger     */
-#define GPIO_MODE_IT_RT		5	/* GPIO Input mode rising edge trigger      */
-#define GPIO_MODE_IT_FRT	6	/* GPIO Input mode fall-rising edge trigger */
+#define GPIO_MODE_IN    	0 /* GPIO Input mode 		  	      */
+#define GPIO_MODE_OUT   	1 /* GPIO Output mode 	  		      */
+#define GPIO_MODE_ALTFN 	2 /* GPIO Alternate functionality 	      */
+#define GPIO_MODE_ANALOG 	3 /* GPIO Analog mode       		      */
+#define GPIO_MODE_IT_FT		4 /* GPIO Input mode falling edge trigger     */
+#define GPIO_MODE_IT_RT		5 /* GPIO Input mode rising edge trigger      */
+#define GPIO_MODE_IT_FRT	6 /* GPIO Input mode fall-rising edge trigger */
 
 
 /*
  * @GPIO_PIN_OUTPUT_TYPE
  * GPIO pin possible output types
  */
-#define GPIO_OP_TYPE_PP		0	/* GPIO Output type push-pull mode 	*/
-#define GPIO_OP_TYPE_OD		1	/* GPIO Output type open-drain mode */
+#define GPIO_OP_TYPE_PP		0 /* GPIO Output type push-pull mode  */
+#define GPIO_OP_TYPE_OD		1 /* GPIO Output type open-drain mode */
 
 
 /*
@@ -91,26 +91,27 @@ typedef struct
  * @GPIO_PIN_PULL_UP_PULL_DOWN
  * GPIO pin pull-up and pull-down configuration macros
  */
-#define GPIO_PIN_NO_PUPD	0	/* GPIO configuration no pull-up, pull-down */
-#define GPIO_PIN_PU			1	/* GPIO configuration pull-up				*/
-#define GPIO_PIN_PD			2	/* GPIO configuration pull-down				*/
+#define GPIO_PIN_NO_PUPD	0 /* GPIO configuration no pull-up, pull-down */
+#define GPIO_PIN_PU		1 /* GPIO configuration pull-up		      */
+#define GPIO_PIN_PD		2 /* GPIO configuration pull-down	      */
 
 
 /******************************************************************************************************************
- * 										APIs supported by this driver											  *
- * 					For more information about the APIs check the function definitions							  *
+ * 					APIs supported by this driver											  *
+ * 			For more information about the APIs check the function definitions							  *
  ******************************************************************************************************************/
-
 /*
  * Peripheral Clock setup
  */
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+
 
 /*
  * Init and De-Init
  */
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle);
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
+
 
 /*
  * Data read and write
@@ -120,6 +121,7 @@ uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value);
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+
 
 /*
  * IRQ Configuration and ISR handling
