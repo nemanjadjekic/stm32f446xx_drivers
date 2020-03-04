e18ce29437a6263e6983ba151491672704a187c8
Nemanja Đekić <nemanjadjekic93@gmail.com>
2020-03-04 12:48:49 +0100

    Working on alignment in .c and .h files

--- Drivers/inc/stm32f446xx_usart_driver.h
+++ Drivers/inc/stm32f446xx_usart_driver.h
@@ -31,12 +31,12 @@ typedef struct
 {
 	USART_RegDef_t *pUSARTx;	/* This holds the base address of USARTx(x:0,1,2) peripheral */
 	USART_Config_t USART_Config;
-	uint8_t *pTxBuffer;			/* Stores application Tx buffer address */
-	uint8_t *pRxBuffer;			/* Stores application Rx buffer address */
-	uint32_t TxLen;				/* Stores Tx length					    */
-	uint32_t RxLen;				/* Stores Rx length					    */
-	uint8_t TxBusyState;		/* Transmission is in  busy state	    */
-	uint8_t RxBusyState;		/* Receiving is in  busy state	   	    */
+	uint8_t *pTxBuffer;		/* Stores application Tx buffer address */
+	uint8_t *pRxBuffer;		/* Stores application Rx buffer address */
+	uint32_t TxLen;			/* Stores Tx length			*/
+	uint32_t RxLen;			/* Stores Rx length			*/
+	uint8_t TxBusyState;		/* Transmission is in  busy state	*/
+	uint8_t RxBusyState;		/* Receiving is in  busy state	   	*/
 }USART_Handle_t;
 
 
@@ -44,27 +44,27 @@ typedef struct
  *@USART_Mode
  *Possible options for USART_Mode
  */
-#define USART_MODE_ONLY_TX 		0
-#define USART_MODE_ONLY_RX 		1
-#define USART_MODE_TXRX    		2
+#define USART_MODE_ONLY_TX 	0
+#define USART_MODE_ONLY_RX 	1
+#define USART_MODE_TXRX    	2
 
 
 /*
  *@USART_Baud
  *Possible options for USART_Baud
  */
-#define USART_STD_BAUD_1200					1200
-#define USART_STD_BAUD_2400					2400
-#define USART_STD_BAUD_9600					9600
-#define USART_STD_BAUD_19200 				19200
-#define USART_STD_BAUD_38400 				38400
-#define USART_STD_BAUD_57600 				57600
-#define USART_STD_BAUD_115200 				115200
-#define USART_STD_BAUD_230400 				230400
-#define USART_STD_BAUD_460800 				460800
-#define USART_STD_BAUD_921600 				921600
-#define USART_STD_BAUD_2M 					2000000
-#define SUART_STD_BAUD_3M 					3000000
+#define USART_STD_BAUD_1200		1200
+#define USART_STD_BAUD_2400		2400
+#define USART_STD_BAUD_9600		9600
+#define USART_STD_BAUD_19200 		19200
+#define USART_STD_BAUD_38400 		38400
+#define USART_STD_BAUD_57600 		57600
+#define USART_STD_BAUD_115200 		115200
+#define USART_STD_BAUD_230400 		230400
+#define USART_STD_BAUD_460800 		460800
+#define USART_STD_BAUD_921600 		921600
+#define USART_STD_BAUD_2M 		2000000
+#define SUART_STD_BAUD_3M 		3000000
 
 
 /*
@@ -98,18 +98,18 @@ typedef struct
  *@USART_HWFlowControl
  *Possible options for USART_HWFlowControl
  */
-#define USART_HW_FLOW_CTRL_NONE    		0
-#define USART_HW_FLOW_CTRL_CTS    		1
-#define USART_HW_FLOW_CTRL_RTS    		2
-#define USART_HW_FLOW_CTRL_CTS_RTS		3
+#define USART_HW_FLOW_CTRL_NONE    	0
+#define USART_HW_FLOW_CTRL_CTS    	1
+#define USART_HW_FLOW_CTRL_RTS    	2
+#define USART_HW_FLOW_CTRL_CTS_RTS	3
 
 
 /*
  * USART Flags
  */
-#define USART_FLAG_TXE 			( 1 << USART_SR_TXE  )
-#define USART_FLAG_RXNE 		( 1 << USART_SR_RXNE )
-#define USART_FLAG_TC 			( 1 << USART_SR_TC   )
+#define USART_FLAG_TXE 		( 1 << USART_SR_TXE  )
+#define USART_FLAG_RXNE 	( 1 << USART_SR_RXNE )
+#define USART_FLAG_TC 		( 1 << USART_SR_TC   )
 
 
 /*
@@ -126,18 +126,20 @@ typedef struct
 #define		USART_EVENT_IDLE       2
 #define		USART_EVENT_CTS        3
 #define		USART_EVENT_PE         4
-#define		USART_ERR_FE     	   5
-#define		USART_ERR_NE    	   6
+#define		USART_ERR_FE           5
+#define		USART_ERR_NE	       6
 #define		USART_ERR_ORE          7
 
+
 /******************************************************************************************
- *								APIs supported by this driver
+ *				APIs supported by this driver
  *		 For more information about the APIs check the function definitions
  ******************************************************************************************/
 /*
  * Peripheral Clock setup
  */
 void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi);
+
 
 /*
  * Init and De-init
