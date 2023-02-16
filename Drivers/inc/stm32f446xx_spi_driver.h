3a4c5d8739ec2f7af4ac3026381203a0e9f1e0be
nemanjadjekic <nemanjadjekic93@gmail.com>
2023-02-16 21:22:36 +0100

    Drivers: Format stm32f446xx_spi_driver.h

--- Drivers/inc/stm32f446xx_spi_driver.h
+++ Drivers/inc/stm32f446xx_spi_driver.h
@@ -15,13 +15,13 @@
  */
 typedef struct
 {
-	uint8_t SPI_DeviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_SclkSpeed;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_SSM;
+    uint8_t SPI_DeviceMode;
+    uint8_t SPI_BusConfig;
+    uint8_t SPI_SclkSpeed;
+    uint8_t SPI_DFF;
+    uint8_t SPI_CPOL;
+    uint8_t SPI_CPHA;
+    uint8_t SPI_SSM;
 }SPI_Config_t;
 
 
@@ -30,95 +30,95 @@ typedef struct
  */
 typedef struct
 {
-	SPI_RegDef_t *pSPIx;	     /* This holds the base address of SPIx(x:0,1,2) peripheral */
-	SPI_Config_t SPIConfig;
-	uint8_t 	 *pTxBuffer; /* Store application Tx buffer address */
-	uint8_t 	 *pRxBuffer; /* Store application Rx buffer address */
-	uint32_t 	 TxLen;      /* Store Tx length			    */
-	uint32_t	 RxLen;      /* Store Rx length			    */
-	uint32_t 	 TxState;    /* Store Tx state			    */
-	uint32_t	 RxState;    /* Store Rx state			    */
+    SPI_RegDef_t *pSPIx;        /* This holds the base address of SPIx(x:0,1,2) peripheral */
+    SPI_Config_t SPIConfig;
+    uint8_t      *pTxBuffer;    /* Store application Tx buffer address                     */
+    uint8_t      *pRxBuffer;    /* Store application Rx buffer address                     */
+    uint32_t     TxLen;         /* Store Tx length                                         */
+    uint32_t     RxLen;         /* Store Rx length                                         */
+    uint32_t     TxState;       /* Store Tx state                                          */
+    uint32_t     RxState;       /* Store Rx state                                          */
 }SPI_Handle_t;
 
 
 /*
  * Possible SPI Application states
  */
-#define SPI_READY		0
-#define SPI_BUSY_IN_RX		1
-#define SPI_BUSY_IN_TX		2
+#define SPI_READY           0
+#define SPI_BUSY_IN_RX      1
+#define SPI_BUSY_IN_TX      2
 
 
 /*
  * Possible SPI Application events
  */
-#define SPI_EVENT_TX_CMPLT	1
-#define SPI_EVENT_RX_CMPLT	2
-#define SPI_EVENT_OVR_ERR	3
-#define SPI_EVENT_CRC_ERR	4
+#define SPI_EVENT_TX_CMPLT      1
+#define SPI_EVENT_RX_CMPLT      2
+#define SPI_EVENT_OVR_ERR       3
+#define SPI_EVENT_CRC_ERR       4
 
 
 /*
  * @SPI_DeviceMode
  */
-#define SPI_DEVICE_MODE_MASTER	1
-#define SPI_DEVICE_MODE_SLAVE	0
+#define SPI_DEVICE_MODE_MASTER      1
+#define SPI_DEVICE_MODE_SLAVE       0
 
 
 /*
  * @SPI_BusConfig
  */
-#define SPI_BUS_CONFIG_FD		1 /* SPI Full-duplex bus configuration 		*/
-#define SPI_BUS_CONFIG_HD		2 /* SPI Half-duplex bus configuration 		*/
-#define SPI_BUS_CONFIG_SIMPLEX_RX_ONLY 	3 /* SPI Simplex bux receive only configuration */
+#define SPI_BUS_CONFIG_FD               1 /* SPI Full-duplex bus configuration          */
+#define SPI_BUS_CONFIG_HD               2 /* SPI Half-duplex bus configuration          */
+#define SPI_BUS_CONFIG_SIMPLEX_RX_ONLY  3 /* SPI Simplex bux receive only configuration */
 
 
 /*
  * @SPI_SclkSpeed
  */
-#define SPI_SCLK_SPEED_DIV2		0 /* Baud rate control: fPCLK/2   */
-#define SPI_SCLK_SPEED_DIV4		1 /* Baud rate control: fPCLK/4   */
-#define SPI_SCLK_SPEED_DIV8		2 /* Baud rate control: fPCLK/8   */
-#define SPI_SCLK_SPEED_DIV16		3 /* Baud rate control: fPCLK/16  */
-#define SPI_SCLK_SPEED_DIV32		4 /* Baud rate control: fPCLK/32  */
-#define SPI_SCLK_SPEED_DIV64		5 /* Baud rate control: fPCLK/64  */
-#define SPI_SCLK_SPEED_DIV128		6 /* Baud rate control: fPCLK/128 */
-#define SPI_SCLK_SPEED_DIV256		7 /* Baud rate control: fPCLK/256 */
+#define SPI_SCLK_SPEED_DIV2         0 /* Baud rate control: fPCLK/2   */
+#define SPI_SCLK_SPEED_DIV4         1 /* Baud rate control: fPCLK/4   */
+#define SPI_SCLK_SPEED_DIV8         2 /* Baud rate control: fPCLK/8   */
+#define SPI_SCLK_SPEED_DIV16        3 /* Baud rate control: fPCLK/16  */
+#define SPI_SCLK_SPEED_DIV32        4 /* Baud rate control: fPCLK/32  */
+#define SPI_SCLK_SPEED_DIV64        5 /* Baud rate control: fPCLK/64  */
+#define SPI_SCLK_SPEED_DIV128       6 /* Baud rate control: fPCLK/128 */
+#define SPI_SCLK_SPEED_DIV256       7 /* Baud rate control: fPCLK/256 */
 
 
 /*
  * @SPI_DFF
  */
-#define SPI_DFF_8BITS		0 /* 8-bit DFF is selected for transmission/reception  */
-#define SPI_DFF_16BITS		1 /* 16-bit DFF is selected for transmission/reception */
+#define SPI_DFF_8BITS       0 /* 8-bit DFF is selected for transmission/reception  */
+#define SPI_DFF_16BITS      1 /* 16-bit DFF is selected for transmission/reception */
 
 
 /*
  * @SPI_CPOL
  */
-#define SPI_CPOL_HIGH		1 /* Clock polarity to 1 when IDLE */
-#define SPI_CPOL_LOW		0 /* Clock polarity to 0 when IDLE */
+#define SPI_CPOL_HIGH       1 /* Clock polarity to 1 when IDLE */
+#define SPI_CPOL_LOW        0 /* Clock polarity to 0 when IDLE */
 
 /*
  * @SPI_CPHA
  */
-#define SPI_CPHA_HIGH		1 /* Second clock transition <=> First data capture edge */
-#define SPI_CPHA_LOW		0 /* First clock transition <=> First data capture edge  */
+#define SPI_CPHA_HIGH       1 /* Second clock transition <=> First data capture edge */
+#define SPI_CPHA_LOW        0 /* First clock transition <=> First data capture edge  */
 
 
 /*
  * @SPI_SSM
  */
-#define SPI_SSM_EN		1 /* Software slave management enabled  */
-#define SPI_SSM_DI		0 /* Software slave management disabled */
+#define SPI_SSM_EN      1 /* Software slave management enabled  */
+#define SPI_SSM_DI      0 /* Software slave management disabled */
 
 
 /*
  * SPI related status flag definitions
  */
-#define SPI_FLAG_TXE 	( 1 << SPI_SR_TXE)
-#define SPI_FLAG_RXNE 	( 1 << SPI_SR_RXNE)
-#define SPI_FLAG_BUSY 	( 1 << SPI_SR_BSY)
+#define SPI_FLAG_TXE    ( 1 << SPI_SR_TXE)
+#define SPI_FLAG_RXNE   ( 1 << SPI_SR_RXNE)
+#define SPI_FLAG_BUSY   ( 1 << SPI_SR_BSY)
 
 
 /******************************************************************************************************************
